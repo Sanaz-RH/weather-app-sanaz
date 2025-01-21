6cb80537418301230dabbd0ef3e4bada0c20899c
Sanaz-RH <149827517+Sanaz-RH@users.noreply.github.com>
2025-01-22 00:48:12 +0100

    Added search function using API

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#city");
+
+  city.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "5e1b950a80to97b3c4f6a5da783801a9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("click", handleSearch);
+
+searchCity("Zurich");
